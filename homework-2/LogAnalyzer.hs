674b89296ab53df64b6e49917997f7a13bee6619
Felipe Nakandakari <felipe@nakand.com>
2015-02-12 14:45:04 -0200

    impl to homework 2 exercises 2 to 5

--- homework-2/LogAnalyzer.hs
+++ homework-2/LogAnalyzer.hs
@@ -18,3 +18,32 @@ parseMessage m = case (words m) of
 parse :: String -> [LogMessage]
 parse s = let ls = lines s
           in map (parseMessage) ls
+
+-- Putting the logs in order
+
+-- Exercise 2
+
+insert :: LogMessage -> MessageTree -> MessageTree
+insert (Unknown _) mTree = mTree
+insert logM Leaf         = Node Leaf logM Leaf
+insert logM@(LogMessage _ t _) (Node leftT tLogM@(LogMessage _ tlts _) rightT)
+    | t <= tlts = Node (insert logM leftT) tLogM rightT
+    | otherwise = Node leftT tLogM (insert logM rightT)
+
+-- Exercise 3
+
+build :: [LogMessage] -> MessageTree
+build []     = Leaf
+build (x:xs) = insert x (build xs)
+
+-- Exercise 4
+
+inOrder :: MessageTree -> [LogMessage]
+inOrder Leaf                      = []
+inOrder (Node Leaf logM Leaf)     = [logM]
+inOrder (Node leftT logM rightT)  = (inOrder leftT) ++ logM:(inOrder rightT)
+
+-- Exercise 5
+
+whatWentWrong :: [LogMessage] -> [String]
+whatWentWrong xs = [ s | LogMessage (Error x) _ s <- inOrder (build xs), x >= 50]
