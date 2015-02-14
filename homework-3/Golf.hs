501e0ad48d883a8cb4e1554b777b2ea9049102f6
Felipe Nakandakari <felipe@nakand.com>
2015-02-14 02:29:38 -0200

    homework-3: improve implementation of exercise 1

--- homework-3/Golf.hs
+++ homework-3/Golf.hs
@@ -1,8 +1,7 @@
 module Golf where
 
 skips :: [a] -> [[a]]
-skips [] = []
-skips xs = xs : map (nthElems xs) [2..length xs]
+skips xs = map (nthElems xs) [1..length xs]
 
 nthElems :: [a] -> Int -> [a]
 nthElems xs p = case drop (p-1) xs of
