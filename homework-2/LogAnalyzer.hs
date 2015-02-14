f1c29012861f3565c5b4e18c94404a534086cc65
Felipe Nakandakari <felipe@nakand.com>
2015-02-14 02:42:09 -0200

    homework 2: remove useless parenthesis

--- homework-2/LogAnalyzer.hs
+++ homework-2/LogAnalyzer.hs
@@ -9,7 +9,7 @@ import Log
 -- Exercise 1 - Log file parsing
 
 parseMessage :: String -> LogMessage
-parseMessage m = case (words m) of
+parseMessage m = case words m of
                     "I":x:s   -> LogMessage Info (read x) (unwords s)
                     "W":x:s   -> LogMessage Warning (read x) (unwords s)
                     "E":x:y:s -> LogMessage (Error (read x)) (read y) (unwords s)
