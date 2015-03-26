3297d67e7b9f8daaeb45ef3b8e2ad88c946fb8ba
Felipe Nakandakari <felipe@nakand.com>
2015-03-26 08:07:29 -0300

    homework-3: first impl to exercise 3

--- homework-3/Golf.hs
+++ homework-3/Golf.hs
@@ -1,5 +1,7 @@
 module Golf where
 
+import Data.List
+
 -- Homework 3
 
 -- Exercise 1
@@ -19,3 +21,26 @@ localMaxima (w:x:y:zs)
     | w < x && x > y = x : localMaxima (x:y:zs)
     | otherwise      = localMaxima (x:y:zs)
 localMaxima _ = []
+
+-- Exercise 3
+
+histogram :: [Integer] -> String
+histogram xs = intercalate "\n" $ (reverse . hLines . countIntL) xs ++ ["==========", "0123456789"]
+
+hLines :: [Integer] -> [String]
+hLines [] = []
+hLines xs = case any (>0) xs of
+    True  -> mapLine xs : (hLines $ decrL xs)
+    False -> []
+
+mapLine :: [Integer] -> String
+mapLine = map (\x -> if x > 0 then '*' else ' ')
+
+countIntL :: [Integer] -> [Integer]
+countIntL xs = map (count xs) [0..9]
+
+count :: [Integer] -> Int -> Integer
+count xs x = toInteger $ length $ filter (\y -> (toInteger x) == y) xs
+
+decrL :: [Integer] -> [Integer]
+decrL = map (\x -> if x > 0 then x-1 else x)
