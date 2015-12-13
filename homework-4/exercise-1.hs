d059297c6104fab364a6524880bbb7f09b45aa52
Felipe Nakandakari <felipe.nakandakari@jora.com>
2015-12-13 20:03:27 +1100

    homework-4: impl for fun2' on exercise 1

--- homework-4/exercise-1.hs
+++ homework-4/exercise-1.hs
@@ -17,3 +17,6 @@ fun2 1 = 0
 fun2 n | even n = n + fun2 (n `div` 2)
        | otherwise = fun2 (3 * n + 1)
 
+fun2' :: Integer -> Integer
+fun2' = sum . filter even . takeWhile (/=1) .
+        iterate (\x -> if even x then x `div` 2 else 3 * x + 1)
